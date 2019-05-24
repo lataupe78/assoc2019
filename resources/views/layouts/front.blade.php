bf4501c117814b445180ac6971faded498d93f75
force.rouge <force.rouge.1978@gmail.com>
2019-05-24 20:10:54 +0200

    adding front layout and updated readme.md

---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<!-- CSRF Token -->
+	<meta name="csrf-token" content="{{ csrf_token() }}">
+
+	<title> @yield('title', config('app.name'))</title>
+
+	<!-- Fonts -->
+	<link rel="dns-prefetch" href="//fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+
+	<!-- Styles -->
+	<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+	@yield('css')
+
+	<!-- Scripts -->
+	@yield('scripts')
+
+</head>
+<body>
+	<div id="app">
+		@include('layouts.front._navbar')
+
+		@yield('top-content')
+
+		<main class="container py-4">
+			@include('layouts.partials._alerts')
+			@include('layouts.partials._breadcrumbs')
+
+			@yield('content')
+		</main>
+	</div>
+	<footer class="bg-dark text-white py-5">
+		@yield('top-footer')
+		<div class="container">
+			@yield('bottom-footer-top')
+			<div class="row">
+
+				<div class="col-sm-6">
+					<p class="h5">{{ config('app.name') }}</p>
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Placeat blanditiis necessitatibus repudiandae, voluptates maiores veritatis libero temporibus nam, odit dolores quidem esse nostrum officiis delectus eos sunt ducimus quas. Provident!</p>
+				</div>
+
+				<div class="col-sm-6">
+					<?php /*
+					@include('front.contacts._form', [
+					'contact' => new App\Contact,
+					'section' => $section ?? new App\Section
+					])
+					*/ ?>
+
+				</div>
+
+			</div>
+			@yield('footer-bottom')
+		</div>
+	</footer>
+
+	<script src="{{ asset('js/app.js') }}"></script>
+	@yield('scripts_bottom')
+
+</body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,99 +1,32 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.front')
 
-        <title>Laravel</title>
+@section('top-content')
+<div class="jumbotron">
+	<div class="container">
+		<h1 class="display-4">Welcome Assoc 2019 test</h1>
+		<p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore earum repellat ipsam nam in provident laborum fuga nemo tenetur, explicabo non praesentium veniam! Numquam ab quos veniam temporibus, asperiores dignissimos!.</p>
+	</div>
+</div>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+@endsection
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
+@section('content')
+<div class="card">
+	<div class="card-header">Un petit rappel du git flow basique:</div>
+	<div class="card-body">
+		<ul>
+			<li>git add .</li>
+			<li>git status</li>
+			<li>git commit -m "Message for the commit"</li>
+		</ul>
+		<ul>
+			<li>
+				git push -u origin master
+			</li>
+		</ul>
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+	</div>
+</div>
 
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
-        </div>
-    </body>
-</html>
+@endsection
